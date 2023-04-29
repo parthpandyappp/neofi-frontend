aabf6541fed6f409f1e3ce66f115fcf46373892f
parthpandyappp <parthpandyappp@gmail.com>
2023-04-30 00:56:48 +0530

    setup: routes, assets, layouts, fonts

---
+++ src/data/index.js
@@ -0,0 +1,99 @@
+const cryptocurrencies = [
+    {
+        "name": "Bitcoin",
+        "code": "BTCUSDT",
+        "icon": "https://cryptologos.cc/logos/bitcoin-btc-logo.png"
+    },
+    {
+        "name": "Ethereum",
+        "code": "ETHUSDT",
+        "icon": "https://cryptologos.cc/logos/ethereum-eth-logo.png"
+    },
+    {
+        "name": "Binance Coin",
+        "code": "BNBUSDT",
+        "icon": "https://cryptologos.cc/logos/binance-coin-bnb-logo.png"
+    },
+    {
+        "name": "Cardano",
+        "code": "ADAUSDT",
+        "icon": "https://cryptologos.cc/logos/cardano-ada-logo.png"
+    },
+    {
+        "name": "XRP",
+        "code": "XRPUSDT",
+        "icon": "https://cryptologos.cc/logos/xrp-xrp-logo.png"
+    },
+    {
+        "name": "Dogecoin",
+        "code": "DOGUSDTE",
+        "icon": "https://cryptologos.cc/logos/dogecoin-doge-logo.png"
+    },
+    {
+        "name": "Polkadot",
+        "code": "DOTUSDT",
+        "icon": "https://cryptologos.cc/logos/polkadot-new-dot-logo.png"
+    },
+    {
+        "name": "Bitcoin Cash",
+        "code": "BCHUSDT",
+        "icon": "https://cryptologos.cc/logos/bitcoin-cash-bch-logo.png"
+    },
+    {
+        "name": "Chainlink",
+        "code": "LINUSDTK",
+        "icon": "https://cryptologos.cc/logos/chainlink-link-logo.png"
+    },
+    {
+        "name": "Litecoin",
+        "code": "LTCUSDT",
+        "icon": "https://cryptologos.cc/logos/litecoin-ltc-logo.png"
+    },
+    {
+        "name": "Stellar",
+        "code": "XLMUSDT",
+        "icon": "https://cryptologos.cc/logos/stellar-xlm-logo.png"
+    },
+    {
+        "name": "Filecoin",
+        "code": "FILUSDT",
+        "icon": "https://cryptologos.cc/logos/filecoin-fil-logo.png"
+    },
+    {
+        "name": "Theta Network",
+        "code": "THEUSDTTA",
+        "icon": "https://cryptologos.cc/logos/theta-theta-logo.png"
+    },
+    {
+        "name": "Bitcoin SV",
+        "code": "BSVUSDT",
+        "icon": "https://cryptologos.cc/logos/bitcoin-sv-bsv-logo.png"
+    },
+    {
+        "name": "TRON",
+        "code": "TRXUSDT",
+        "icon": "https://cryptologos.cc/logos/tron-trx-logo.png"
+    },
+    {
+        "name": "EOS",
+        "code": "EOSUSDT",
+        "icon": "https://cryptologos.cc/logos/eos-eos-logo.png"
+    },
+    {
+        "name": "Aave",
+        "code": "AAVUSDTE",
+        "icon": "https://cryptologos.cc/logos/aave-aave-logo.png"
+    },
+    {
+        "name": "Chainlink",
+        "code": "LINUSDTK",
+        "icon": "https://cryptologos.cc/logos/chainlink-link-logo.png"
+    },
+    {
+        "name": "Polkadot",
+        "code": "DOTUSDT",
+        "icon": "https://cryptologos.cc/logos/polkadot-new-dot-logo.png"
+    }
+]
+
+export { cryptocurrencies }
---
+++ src/routes/Endpoints.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages";
+
+const Endpoints = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
+
+export { Endpoints as Routes };
